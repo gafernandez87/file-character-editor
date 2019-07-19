6f137ae84155c7188fd8223fa93c8ed47095abcd
Gaston Fernandez <gaston.fernandez@softvision.com>
2019-07-18 21:30:06 -0300

    index

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const app = express();
 app.use(bodyParser.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
     res.send("Hello");    
 })
 
 app.get('*', (_, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
 
